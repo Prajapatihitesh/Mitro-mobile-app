dd27424b1146168809ea0991ac5d43745eefa574
hitesh prajapati <124128633+Prajapatihitesh@users.noreply.github.com>
2023-06-21 19:02:05 +0530

    Delete models directory

--- models/chatUserModels.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class ChatUsers{
-  String name;
-  String imageURL;
-  String time;
-  ChatUsers({ required this.name,required this.imageURL,required this.time});
-}

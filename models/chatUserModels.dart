c39887df9425888f0ab1c2eb9a67a0bd2b3c7e4d
hitesh prajapati <124128633+Prajapatihitesh@users.noreply.github.com>
2023-06-21 18:58:22 +0530

    Add files via upload

---
+++ models/chatUserModels.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatUsers{
+  String name;
+  String imageURL;
+  String time;
+  ChatUsers({ required this.name,required this.imageURL,required this.time});
+}
